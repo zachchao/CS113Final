135a04a7e9dcce04ef261f7c5895f82aaa1413f7
Zachary Chao <zach.chao@yahoo.com>
2017-12-02 11:58:35 -0800

    first commit

---
+++ src/edu/miracosta/cs113/Test.java
@@ -0,0 +1,14 @@
+package edu.miracosta.cs113;
+
+public class Test {
+	public static void main(String[] args){
+		int[][] maze = new int[5][5];
+		
+		maze[0] = new int[] {0,1,1,1,0};
+		maze[1] = new int[] {0,0,0,0,0};
+		maze[2] = new int[] {0,0,1,0,0};
+		maze[3] = new int[] {0,1,1,1,0};
+		maze[4] = new int[] {0,0,0,0,0};
+		
+	}
+}
